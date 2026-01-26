da3e7526aae1dd4e15fda3b4ee55f6689139cbbe
BillWan-zzzyyy <2297772051@qq.com>
2026-01-26 17:49:28 -0600

    update video

--- vehicle_speed_estimator/config/settings.py
+++ vehicle_speed_estimator/config/settings.py
@@ -1,6 +1,6 @@
 # 视频路径配置
 # 方式1: 使用本地视频文件（推荐）- 直接指定本地视频文件的完整路径
-VIDEO_PATH = r"D:\OneDrive - UW-Madison\Project\alpha_stage_2\yolov11-vehicle-speed-tracker\vehicle_speed_estimator\assets\testcrash.mp4"
+VIDEO_PATH = r"assets\testcrash.mp4"
 
 # 方式2: 从网络下载视频 - 如果 VIDEO_PATH 为 None，则使用以下配置下载
 VIDEO_URL = "https://drive.google.com/uc?export=download&id=1fYb05GW0sWeI1EiTfcbjOtGBrdIuPq4U"
