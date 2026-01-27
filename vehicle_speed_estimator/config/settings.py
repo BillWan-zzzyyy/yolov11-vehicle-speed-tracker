8b693fb470fdbd12a5617b707b31dec451db7063
BillWan-zzzyyy <2297772051@qq.com>
2026-01-26 23:06:44 -0600

    update constant

--- vehicle_speed_estimator/config/settings.py
+++ vehicle_speed_estimator/config/settings.py
@@ -1,5 +1,5 @@
 #default for test
-VIDEO_PATH = r"assets\test3.mp4"
+VIDEO_PATH = r"assets\test2.mp4"
 
 #no downloaded video
 VIDEO_URL = "https://drive.google.com/uc?export=download&id=1fYb05GW0sWeI1EiTfcbjOtGBrdIuPq4U"
@@ -10,4 +10,4 @@ VIDEO_DOWNLOAD_PATH = "assets"  # 下载视频保存的文件夹
 MODEL_PATH = "models/yolo11n.pt"
 MPS_TO_KPH = 3.6
 CLASSES_TO_TRACK = [1, 2, 5, 7]  # bicycle， car, bus, truck
-CONFIDENCE_THRESHOLD = 0.4
+CONFIDENCE_THRESHOLD = 0.3
